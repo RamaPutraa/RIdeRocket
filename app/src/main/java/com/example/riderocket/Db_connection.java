d5e1a200c2d571c82b4367ea8c0156268d49c8cc
indraudyn <email-indraudayana02@gmail.com>
2024-07-04 16:07:27 +0800

    membuat tampilan looping untuk user

--- app/src/main/java/com/example/riderocket/Db_connection.java
+++ app/src/main/java/com/example/riderocket/Db_connection.java
@@ -1,7 +1,7 @@
 package com.example.riderocket;
 
 public class Db_connection {
-    public static  String ip = "192.168.1.10";
+    public static  String ip = "192.168.1.8";
 
     public static final String urlLogin = "http://"+ip+"/riderocket/api_login.php";
     public static final String urlRegister = "http://"+ip+"/riderocket/api_register.php";
